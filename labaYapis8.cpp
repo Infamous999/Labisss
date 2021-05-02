2b825ffe3bf1197618604492b6282eda603b1a05
Infamous999 <79654330+Infamous999@users.noreply.github.com>
2021-05-02 12:30:04 +0600

    Add files via upload

---
+++ labaYapis8.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+
+class Vector {
+	double coords[3];
+public:
+	double& operator[](int i) {
+		int k = i % 3;
+		return coords[k];
+	}
+	
+	Vector operator()(double x, double y, double z) {
+		coords[0] = x;
+		coords[1] = y;
+		coords[2] = z;
+		return *this;
+	}
+	
+	Vector operator()(Vector a, Vector b) {
+		for (int i = 0; i < 3; i++)
+			coords[i] = a[i + 1] * b[i + 2] - a[i + 2] * b[i + 1];
+		return *this;
+	}
+	
+	double operator*(Vector obj) {
+		double res = 0;
+		for (int i = 0; i < 3; i++) res += coords[i] * obj[i];
+		return res;
+	}
+	
+	Vector(double x, double y, double z) {
+		coords[0] = x;
+		coords[1] = y;
+		coords[2] = z;
+	}
+	
+	void show() {
+		cout << "(" << coords[0] << ", " << coords[1] << ", " << coords[2] << ")\n";
+	}
+	
+	Vector() {
+		for (int i = 0; i < 3; i++)
+			coords[i] = 0;
+	}
+};
+int main() {
+	
+	Vector a, b(3, 8, 7), c;
+	a(1, -5, 9);
+	a.show();
+	b.show();
+	cout << "a*b=" << a * b << endl;
+	c(a, b);
+	cout << "[ab]=";
+	c.show();
+	return 0;
+}
