43dfef644c74a3e542b476888f3e4bb3a01d9684
Infamous999 <79654330+Infamous999@users.noreply.github.com>
2021-06-02 00:25:49 +0600

    Add files via upload

---
+++ ConsoleApplication10.cpp
@@ -0,0 +1,129 @@
+﻿#include <windows.h>
+#include <iostream>
+using namespace std;
+
+void prob(int i)
+{
+    for (int k = 0; k < i; k++)
+        cout << ' ';
+}
+
+void zvezd(int i)
+{
+    for (int k = 0; k < i; k++)
+        cout << '*'; 
+}
+
+void clear(int i)
+{
+    for (int k = 0; k < i; k++)
+        cout << endl;
+}
+
+int main()
+{
+    /* Без анимации
+****   *    * *     *
+*      *  *   **    *
+***    * *    * *   *
+*      *  *   *  *  *
+*      *    * *    **
+                                                    */
+
+    for (int i = 1; i < 4; i++)
+    {
+        
+        zvezd(4 * i);
+        prob(3 * i);
+        zvezd(1 * i);
+        prob(4 * i);
+        zvezd(1 * i);
+        prob(1 * i);
+        zvezd(1 * i);
+        prob(5 * i);
+        zvezd(1 * i);
+        prob(3 * i);
+
+        
+
+        cout << endl;
+
+       
+        zvezd(1 * i);
+        prob(6 * i);
+        zvezd(1 * i);
+        prob(2 * i);
+        zvezd(1 * i);
+        prob(3 * i);
+        zvezd(1 * i);
+        prob(0 * i);
+        zvezd(1 * i);
+        prob(4 * i);
+        zvezd(1 * i);
+        prob(2 * i);
+        
+       
+
+        cout << endl;
+
+       
+        zvezd(3 * i);
+        prob(4 * i);
+        zvezd(1 * i);
+        prob(1 * i);
+        zvezd(1 * i);
+        prob(4 * i);
+        zvezd(1 * i);
+        prob(1 * i);
+        zvezd(1 * i);
+        prob(3 * i);
+        zvezd(1 * i);
+        prob(2 * i);
+        
+
+        cout << endl;
+
+       
+        zvezd(1 * i);
+        prob(6 * i);
+        zvezd(1 * i);
+        prob(2 * i);
+        zvezd(1 * i);
+        prob(3 * i);
+        zvezd(1 * i);
+        prob(2 * i);
+        zvezd(1 * i);
+        prob(2 * i);
+        zvezd(1 * i);
+        prob(2 * i);
+       
+        
+
+        cout << endl;
+
+        
+        zvezd(1 * i);
+        prob(6 * i);
+        zvezd(1 * i);
+        prob(4 * i);
+        zvezd(1 * i);
+        prob(1 * i);
+        zvezd(1 * i);
+        prob(4 * i);
+        zvezd(1 * i);
+        prob(0 * i);
+        zvezd(1 * i);
+        prob(2 * i);
+
+       
+        
+
+        cout << endl;
+
+        clear(19);
+        Sleep(900);
+    }
+
+    system("pause");
+    return 0;
+}
